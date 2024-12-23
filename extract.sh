7aee9083885414acce182edf69318a2a1eb1af86
IbinMas <nagy-mas@protonmail.com>
2024-12-23 02:02:17 +0000

    add extrat.sh for trobleshooting

--- extract.sh
+++ extract.sh
@@ -28,33 +28,6 @@ echo "Contents of pve-cluster-backup:"
 ls -l extracted/pve-cluster
 
 
-# # Navigate to the directory containing the tarball
-# cd /mnt/pve/media/ROXMOX_BACKUP
-
-# # Ensure the target extraction directory exists
-# mkdir -p /mnt/pve/media/ROXMOX_BACKUP/back-test
-
-# # Extract the tarball to the target directory
-# tar -xzf pve_pve_2024-12-18.19.20.32.tar.gz -C /mnt/pve/media/ROXMOX_BACKUP/back-test
-
-# # Verify the extracted files
-# ls -l /mnt/pve/media/ROXMOX_BACKUP/back-test
-
-
-
-# Check the status of the pve-cluster service
-systemctl status pve-cluster.service
-
-# View detailed logs for the pve-cluster service
-journalctl -xeu pve-cluster.service
-
-# Verify permissions of restored files and directories
-ls -l /etc/pve
-ls -l /var/lib/pve-cluster
-
-# Manually restart the pve-cluster service
-systemctl restart pve-cluster.service
-
 
 
 
